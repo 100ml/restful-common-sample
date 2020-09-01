f4bac99ca7e18a82da5f5985fd3b9b705e7e8f0c
will <everyday100ml@gmail.com>
2020-09-01 15:20:58 +0800

    modification: simplify code

--- src/main/java/com/threadclub/sample/controller/SampleV2Controller.java
+++ src/main/java/com/threadclub/sample/controller/SampleV2Controller.java
@@ -1,18 +1,13 @@
 package com.threadclub.sample.controller;
 
 import com.threadclub.controller.SimpleController;
-import com.threadclub.sample.controller.vo.SampleVo;
 import com.threadclub.sample.entity.Sample;
 import com.threadclub.sample.service.SampleService;
-import com.threadclub.sample.service.dto.SampleAddDto;
-import com.threadclub.sample.service.dto.SampleQueryDto;
-import com.threadclub.sample.service.dto.SampleUpdateDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
-import java.util.function.Function;
 
 @RestController
 @RequestMapping("/samples/v2")
@@ -23,12 +18,6 @@ public class SampleV2Controller extends SimpleController {
 
     @PostConstruct
     protected void setBaseService() {
-        Function<Sample, SampleVo> voFunction = x -> new SampleVo(x);
-        super.entityType(Sample.class)
-                .updateDtoType(SampleUpdateDto.class)
-                .addDtoType(SampleAddDto.class)
-                .queryDtoType(SampleQueryDto.class)
-                .service(sampleService)
-                .voConverter(voFunction);
+        super.init(Sample.class, sampleService);
     }
 }
--- src/main/java/com/threadclub/sample/controller/vo/SampleVo.java
+++ src/main/java/com/threadclub/sample/controller/vo/SampleVo.java
@@ -1,15 +1,26 @@
 package com.threadclub.sample.controller.vo;
 
+import com.threadclub.controller.vo.BaseVo;
 import com.threadclub.sample.entity.Sample;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class SampleVo {
+@NoArgsConstructor
+public class SampleVo extends BaseVo<Sample> {
 
     private String name;
     private String color;
 
     public SampleVo(Sample sample) {
+        if (sample != null) {
+            this.name = sample.getName();
+            this.color = sample.getColor();
+        }
+    }
+
+    @Override
+    public void copy(Sample sample) {
         if (sample != null) {
             this.name = sample.getName();
             this.color = sample.getColor();
